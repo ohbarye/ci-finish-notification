fe9511cfd666eb15c2c78ee791e0d22406e57bfc
ohbarye <over.rye@gmail.com>
2016-01-23 17:45:29 +0900

    Not to use forEach for Node list

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,11 @@
   var targetNodes = document.querySelectorAll('.octicon-primitive-dot');
 
   var observer = new MutationObserver(function(mutations, self) {
-    alert('CI finished!')
+    alert('CI finished!');
   });
 
-  targetNodes.forEach(function(node) {
-    observer.observe(node, { childList: true, subtree: true });
-  });
+  for(var i = 0; i < targetNodes.length; i++) {
+    observer.observe(targetNodes[i], { childList: true, subtree: true });
+  };
 
 }).call(this);
